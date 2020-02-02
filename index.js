0534180902ddca9e22801757e32b35a9865d7743
Rizwan Tariq <rizwantariq95@outlook.com>
2020-02-03 02:49:14 +0500

    Added Post & Update services

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Joi = require("joi");
 const express = require("express");
-
 const app = express();
 
 //MiddleWares
@@ -13,6 +12,7 @@ const genres = [
 ];
 
 //Services
+//GET
 app.get("/api/genres/", (req, res) => {
   res.send(genres);
 });
@@ -22,10 +22,35 @@ app.get("/api/genres/:id", (req, res) => {
   res.send(genre);
 });
 
+//POST
+app.post("/api/genres/", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = { id: genres.length + 1, name: req.body.name };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+//UPDATE || PUT
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("No genre found...");
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genres);
+});
+
 //Validation Function
 function validateGenre(genre) {
   const schema = {
-    name: Joi.string().min(3).required
+    name: Joi.string()
+      .min(3)
+      .required()
   };
   return Joi.validate(genre, schema);
 }
