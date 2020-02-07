ec713ad75ab95dc10de11a1f049ca34ac8f337b8
Rizwan Tariq <rizwantariq95@outlook.com>
2020-02-07 18:52:09 +0500

    Structure Genres in seprate route

--- index.js
+++ index.js
@@ -1,69 +1,10 @@
-const Joi = require("joi");
 const express = require("express");
+const genres = require("./routes/genres");
 const app = express();
 
 //MiddleWares
 app.use(express.json());
-
-const genres = [
-  { id: 1, name: "Comedy" },
-  { id: 2, name: "Action" },
-  { id: 3, name: "Horror" }
-];
-
-//Services
-//GET
-app.get("/api/genres/", (req, res) => {
-  res.send(genres);
-});
-app.get("/api/genres/:id", (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("No genre found...");
-  res.send(genre);
-});
-
-//POST
-app.post("/api/genres/", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const genre = { id: genres.length + 1, name: req.body.name };
-
-  genres.push(genre);
-  res.send(genre);
-});
-
-//UPDATE || PUT
-app.put("/api/genres/:id", (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("No genre found...");
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  genre.name = req.body.name;
-  res.send(genres);
-});
-
-//DELETE
-app.delete("/api/genres/:id", (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("No genre found...");
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send(genre);
-});
-
-//Validation Function
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-  return Joi.validate(genre, schema);
-}
+app.use("/api/genres", genres);
 
 //SettingUp port
 const port = process.env.PORT || 3000;
