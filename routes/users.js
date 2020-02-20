2b53a5aea61088c76551fff1ceef89a93e3ee1b4
Rizwan Tariq <rizwantariq95@outlook.com>
2020-02-20 16:04:57 +0500

    Password hashing implemented using bcrypt package

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const _ = require("lodash");
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { User, validate } = require("../models/user");
 
 const router = express.Router();
@@ -8,13 +9,18 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  //
+  //validate Password
   if (!req.body.password) return res.status(400).send("Password is required..");
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered..");
+  //
 
   user = new User(_.pick(req.body, ["name", "email", "password"]));
+  //Hashing Password
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
   await user.save();
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
