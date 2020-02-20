9971dbfc188f18230d9ce3d28a1ac835ccd6694e
Rizwan Tariq <rizwantariq95@outlook.com>
2020-02-20 16:58:32 +0500

    User authentication service implemented

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const movies = require("./routes/movies");
 const customers = require("./routes/customers");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 
 const app = express();
 mongoose
@@ -27,6 +28,7 @@ app.use("/api/movies", movies);
 app.use("/api/customers", customers);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 //SettingUp port
 const port = process.env.PORT || 3000;
